27e421b3e6f90c6843bb09b9ba65b6b13ee17389
Jon Ludlam <jon@xcp-dev>
2011-08-17 15:31:38 +0100

    Set version numbers based on the current time as a prelude to setting up a debian repository

--- build.sh
+++ build.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 
 TOP=`pwd`
-
+BUILDTIME=`date +'%s'`
+STDVER=0.1+${BUILDTIME}
 mkdir tmp-build
 mkdir tmp-checkout
 
 # Build xen
 
-./build_deb.sh xen 4.1.1 git://github.com/jonludlam/xen-debian.git xen_4.1.1.orig-qemu.tar.gz
+./build_deb.sh -p xen -v 4.1.1 -g git://github.com/jonludlam/xen-debian.git -e xen_4.1.1.orig-qemu.tar.gz
 
 # Build userspace blktap
 git clone git://github.com/jonludlam/blktap.git tmp-checkout/blktap
-./mk_git_archive.sh tmp-checkout/blktap blktap master 0.1 $TOP/pristine
-./build_deb.sh blktap 0.1 git://github.com/jonludlam/blktap-debian.git 
+./mk_git_archive.sh tmp-checkout/blktap blktap master 1.0.0+${BUILDTIME} $TOP/pristine
+./build_deb.sh -p blktap -v 1.0.0+${BUILDTIME} -g git://github.com/jonludlam/blktap-debian.git
 
 # Xen-api-libs
 git clone git://github.com/xen-org/xen-api-libs.git tmp-checkout/xen-api-libs
-./mk_git_archive.sh tmp-checkout/xen-api-libs xen-api-libs master 0.1 $TOP/pristine
-./build_deb.sh xen-api-libs 0.1 git://github.com/jonludlam/xen-api-libs-debian.git
+./mk_git_archive.sh tmp-checkout/xen-api-libs xen-api-libs master ${STDVER} $TOP/pristine
+./build_deb.sh -p xen-api-libs -v ${STDVER} -g git://github.com/jonludlam/xen-api-libs-debian.git
 
 # Xen-api
 git clone git://github.com/xen-org/xen-api.git tmp-checkout/xen-api
-./mk_git_archive.sh tmp-checkout/xen-api xen-api master 0.1 $TOP/pristine
-./build_deb.sh xen-api 0.1 git://github.com/jonludlam/xen-api-debian.git
+./mk_git_archive.sh tmp-checkout/xen-api xen-api master ${STDVER} $TOP/pristine
+./build_deb.sh -p xen-api -v ${STDVER} -g git://github.com/jonludlam/xen-api-debian.git
 
 # Vhdd
 git clone git://github.com/jonludlam/vhdd.git tmp-checkout/vhdd
-./mk_git_archive.sh tmp-checkout/vhdd vhdd master 0.1 $TOP/pristine
-./build_deb.sh vhdd 0.1 git://github.com/jonludlam/vhdd-debian.git
+./mk_git_archive.sh tmp-checkout/vhdd vhdd master ${STDVER} $TOP/pristine
+./build_deb.sh -p vhdd -v ${STDVER} -g git://github.com/jonludlam/vhdd-debian.git 
 
 # Xen-sm
 hg clone http://xenbits.xen.org/XCP/xen-sm.hg tmp-checkout/xen-sm.hg
-./mk_hg_archive.sh tmp-checkout/xen-sm.hg xen-sm 0.1 $TOP/pristine
-./build_deb.sh xen-sm 0.1 git://github.com/jonludlam/xen-sm-debian.git
+./mk_hg_archive.sh tmp-checkout/xen-sm.hg xen-sm ${STDVER} $TOP/pristine
+./build_deb.sh -p xen-sm -v ${STDVER} -g git://github.com/jonludlam/xen-sm-debian.git
 
 # vncterm
 hg clone http://hg/carbon/trunk/vncterm.hg tmp-checkout/vncterm.hg
-./mk_hg_archive.sh tmp-checkout/vncterm.hg vncterm 0.1 $TOP/pristine
-./build_deb.sh vncterm 0.1 git://github.com/jonludlam/vncterm-debian.git
+./mk_hg_archive.sh tmp-checkout/vncterm.hg vncterm ${STDVER} $TOP/pristine
+./build_deb.sh -p vncterm -v ${STDVER} -g git://github.com/jonludlam/vncterm-debian.git 
--- build_deb.sh
+++ build_deb.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
+set -e
+set -x
 
-# $1=pkg 
-# $2=version
-# $3=git repository for debian packaging files
+usage() {
+cat << EOF
+Usage:
+	$(basename $0) [-h] -p <package name> -v <upstream version>
+		-g <git repo for debian dir> [-e <extra source tarball>]
+		[-n new version]
 
-mkdir -p tmp-build/$1
-cd tmp-build/$1
-cp ../../pristine/$1_$2.orig.tar.gz .
-tar zxvf $1_$2.orig.tar.gz
-if [ ! -z $4 ]; then cp ../../pristine/$4 .; fi
-cd $1-$2
-if [ ! -z $4 ]; then tar zxvf ../$4; fi
-git clone $3 debian
+	-p : Package name
+	-v : Upstream version number
+	-g : Git repository url for the debian directory
+	-e : Extra source tarball to put into the src package
+	-n : New version (will be passed to dch)
+	-h : This help screen
+}
+EOF
+}
+
+while getopts "hp:v:g:e:n:" opt; do
+  case $opt in
+    h) usage; exit 0;;
+    p) pkg="${OPTARG}";;
+    v) version="${OPTARG}";;
+    g) gitrepo="${OPTARG}";;
+    e) extratarball="${OPTARG}";;
+    *) echo "Invalid option"; usage ; exit 1 ;; 
+  esac
+done
+
+mkdir -p tmp-build/${pkg}
+cd tmp-build/${pkg}
+cp ../../pristine/${pkg}_${version}.orig.tar.gz .
+tar zxvf ${pkg}_${version}.orig.tar.gz
+if [ ! -z ${extratarball} ]; then cp ../../pristine/${extratarball} .; fi
+cd ${pkg}-${version}
+if [ ! -z ${extratarball} ]; then tar zxvf ../${extratarball}; fi
+git clone ${gitrepo} debian
+currentver=`dpkg-parsechangelog | grep Version | cut -d\  -f2 | cut -d- -f1`
+if [ ${currentver} != ${version} ]; then dch --newversion ${version}-1 "New autobuild version"; fi
+mv debian/.git /tmp/${pkg}_debian_git
 pdebuild --configfile ../../../pbuilderrc --use-pdebuild-internal
+mv /tmp/${pkg}_debian_git debian/.git
 cd ../../..
 
 
