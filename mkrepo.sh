312e9fe3ab0558d3a5d3ed07eb62b842b4b2d0f9
Jon Ludlam <jon@xcp-dev>
2011-08-19 14:07:48 +0100

    Update the create repo stuff to add in the blktap-dkms deb package too

--- mkrepo.sh
+++ mkrepo.sh
@@ -6,4 +6,6 @@ cd tmp-debs
 dpkg-sig --sign builder *.deb
 cd $TOP/repo/apt/debian
 reprepro includedeb unstable $TOP/tmp-debs/*.deb
-
+wget http://downloads.xen.org/XCP/debian/blktap-dkms_0.1_all.deb
+reprepro includedeb unstable blktap-dkms_0.1_all.deb
+rm blktap-dkms_0.1_all.deb
