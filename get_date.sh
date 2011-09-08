09621d15319a121ec0b92a80b6fab8e26475da77
Jon Ludlam <jon@xcp-dev>
2011-09-08 14:55:52 +0100

    Use a more backward-compatible way of getting the date from git

--- get_date.sh
+++ get_date.sh
@@ -5,7 +5,7 @@ set -e
 pushd $1 > /dev/null
 
 if [ -e .git ]; then
-	git log --date raw | grep "^Date" | head -n 1 | cut -c 9- | cut -d\  -f1
+	git show --format=format:%at | head -n 1
 elif [ -e .hg ]; then
 	hg log --template '{date}\n' | cut -d. -f1 | head -n 1
 else
