edf76a6b55a80aceceab6dcba3b817751fb4dbc1
Jon Ludlam <jon@xcp-dev>
2011-09-18 22:38:31 +0100

    Make repo have the source packages in too

--- mkrepo.sh
+++ mkrepo.sh
@@ -6,6 +6,9 @@ cd tmp-debs
 dpkg-sig --sign builder *.deb
 cd $TOP/repo/apt/debian
 reprepro includedeb unstable $TOP/tmp-debs/*.deb
+for i in $TOP/tmp-debs/*.dsc; do
+	reprepro includedsc unstable $i
+done
 wget http://downloads.xen.org/XCP/debian/blktap-dkms_0.1_all.deb
 reprepro includedeb unstable blktap-dkms_0.1_all.deb
 rm blktap-dkms_0.1_all.deb
