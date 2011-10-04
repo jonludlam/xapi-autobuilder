51f5308283d29a0514f35592789d7d42c87eff54
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2011-10-04 16:39:44 +0100

    Always archive HEAD, allowing the checkout to select the branch

--- mk_archive.sh
+++ mk_archive.sh
@@ -4,7 +4,7 @@ pushd $1
 if [ -e .hg ]; then
 	hg archive -t tgz --prefix $2-$4/ $5/$2_$4.orig.tar.gz
 elif [ -e .git ]; then
-	git archive --format tar --prefix=$2-$4/ $3 | gzip - > $5/$2_$4.orig.tar.gz
+	git archive --format tar --prefix=$2-$4/ HEAD | gzip - > $5/$2_$4.orig.tar.gz
 else
 	echo "No git/mercurial repo here!"
 	popd
