4f176f9490f501451c9f0eeed5df20a7771b0bda
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2011-11-02 17:13:19 -0400

    Fix the git-buildpackage problem by making sure that we have the correct branches

--- checkout.sh
+++ checkout.sh
@@ -5,7 +5,13 @@ mkdir -p tmp-checkout
 pushd tmp-checkout
 
 while read line; do
-	git clone $line
+	repo=`echo $line | awk '{print $1}'`
+	branch=`echo $line | awk '{print $2}'`
+	dir=`basename $repo .git`
+	git clone $repo
+	pushd $dir
+	git checkout $branch
+	popd
 done < ../git-repos
 
 for i in `cat ../hg-repos`
