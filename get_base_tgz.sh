dfb73fbe7def96abe1e51135612b4fb967a9ad50
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2011-11-02 22:59:44 +0000

    Create the cowbuilder dir

--- get_base_tgz.sh
+++ get_base_tgz.sh
@@ -3,6 +3,7 @@
 if [ -e /var/cache/pbuilder/$DIST-$ARCH/base.cow ]; then
 	sudo -E cowbuilder --update --configfile pbuilderrc2
 else
+        sudo mkdir -p /var/cache/pbuilder/$DIST-$ARCH/base.cow
         sudo -E cowbuilder --create --configfile pbuilderrc2
 fi
 
