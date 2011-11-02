26b1a46874ffe28faf439dc5f3c33174d4ecff2e
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2011-11-02 22:40:32 +0000

    Fix get_base_tgz following cowbuilder change

--- get_base_tgz.sh
+++ get_base_tgz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -e /var/cache/pbuilder/$DIST-$ARCH-base.tgz ]; then
+if [ -e /var/cache/pbuilder/$DIST-$ARCH/base.cow ]; then
 	sudo -E cowbuilder --update --configfile pbuilderrc2
 else
         sudo -E cowbuilder --create --configfile pbuilderrc2
