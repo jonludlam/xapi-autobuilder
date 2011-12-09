7b8677f279b19521a3aa0972e47390ed66914fbf
Mike McClurg <mike.mcclurg@citrix.com>
2011-12-09 17:12:08 +0000

    Add create-ubuntu-branch.sh script for setting changelog
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

---
+++ kronos-tools/create-ubuntu-branch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+git checkout master
+git fetch origin debian:debian
+git branch -D ubuntu
+git branch ubuntu debian
+git checkout ubuntu
+
+version=$(dpkg-parsechangelog -c1 -ldebian/changelog | grep Version | cut -f2 -d' ')
+dch -v ${version}ubuntu1 "Ubuntu stamp" --distribution "oneiric precise" --force-distribution
