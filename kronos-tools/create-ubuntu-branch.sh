a9bb6e273601c7e7d5b58af1c538f20f553ec69c
Mike McClurg <mike.mcclurg@citrix.com>
2011-12-14 14:08:31 +0000

    First check whether ubuntu branch exists
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

--- kronos-tools/create-ubuntu-branch.sh
+++ kronos-tools/create-ubuntu-branch.sh
@@ -3,13 +3,21 @@
 set -e
 set -x
 
+# If ubuntu branch exists, check it out and use it, as it's being managed
+# manually. Otherwise, create the ubuntu branch from the debian branch.
+
 git checkout master
 git fetch origin debian:debian
-git branch -D ubuntu
+
+# Does ubuntu branch exist? Check it out and exit.
+if [ `git branch | grep "ubuntu$"` ]; then
+	git checkout ubuntu
+	exit 0
+fi
+
+# Ubuntu branch doesn't exist. Create it from debian and bump changelog.
 git branch ubuntu debian
 git checkout ubuntu
 
 version=$(dpkg-parsechangelog -c1 -ldebian/changelog | grep Version | cut -f2 -d' ')
 dch -v ${version}ubuntu1 "Ubuntu stamp" --distribution "oneiric precise" --force-distribution
-
-git commit -a -s -m "Ubuntu stamp"
