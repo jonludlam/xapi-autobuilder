86ca040e1dd88b3b102c02635b08b10626ec9e37
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2012-02-07 15:26:29 +0000

    More fix_networking fixups

--- kronos-test/fix_networking.sh
+++ kronos-test/fix_networking.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 pif=`xe pif-list device=eth0 --minimal`
-ifconfig eth0 0.0.0.0
+killall dhclient
 xe pif-reconfigure-ip uuid=$pif mode=dhcp
+cat > /etc/network/interfaces << EOF
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+EOF
+ifconfig eth0 0.0.0.0
 
